0431b4d1a947b12fca58c46c8ddbe85dda8345c5
Namrata Mahajan <nammahajan12@gmail.com>
2016-11-05 10:51:05 +0530

    Initial Commit

---
+++ NMPhotoGallery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NMPhotoGallery
+//
+//  Created by Namrata on 05/11/16.
+//  Copyright © 2016 Namrata Mahajan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NMPhotoGallery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NMPhotoGallery
+//
+//  Created by Namrata on 05/11/16.
+//  Copyright © 2016 Namrata Mahajan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
